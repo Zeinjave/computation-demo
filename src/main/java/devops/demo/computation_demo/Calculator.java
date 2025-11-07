1cef6ebf3700ffbc53d888575b0ab56d482c75c8
Heather Hays <zein.duat@gmail.com>
2025-11-07 11:53:32 -0600

    fixed failed tests

--- src/main/java/devops/demo/computation_demo/Calculator.java
+++ src/main/java/devops/demo/computation_demo/Calculator.java
@@ -20,16 +20,12 @@ public int multiply(int value1, int value2) {
 
 }
 
-public int divide(int value1, int value2) {
-	
-		return value1 / value2;
-}
+public int divide(int value1, int value2) {  
 
-public int divideByZero(int value1, int value2) {  
-
-        if(value2 == 0)
+        if(value2 == 0) {
 
                throw new IllegalArgumentException("Denominator value cannot be zero.");
+        }
 
         return value1 / value2;
 
